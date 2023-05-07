a4bb243efea901b02034783386892b76629eb20a
JhayStark <jaamen5@gmail.com>
2023-05-07 17:31:34 +0000

    env edits

--- modules/funerals/funeral.controller.js
+++ modules/funerals/funeral.controller.js
@@ -1,6 +1,6 @@
 const Funeral = require("./funeral.model");
 const User = require("../user/user.model");
-const { imageUploader } = require("../../config/cloudinary");
+const { sendSms } = require("../../utils/smsApi");
 
 const createFuneral = async (req, res) => {
   const userId = req.userId;
@@ -130,6 +130,42 @@ const deleteFuneral = async (req, res) => {
   }
 };
 
+const sendMessages = async (req, res) => {
+  const { funeralId } = req.query;
+  let successCount = 0;
+  let failCount = 0;
+  try {
+    const donors = await Funeral.findById(funeralId, { donations: 1 }).populate(
+      "donations",
+      { donorPhoneNumber: 1, _id: 0 }
+    );
+    if (donors.length <= 0) return res.status(404).send("No donors found");
+    if (donors.donations.length > 0) {
+      const donorNumbers = donors.donations;
+      const sending = donorNumbers.map((donor) => {
+        sendSms(donor.donorPhoneNumber)
+          .then((response) => {
+            console.log(response);
+            successCount += 1;
+          })
+          .catch((error) => {
+            console.log(error);
+            failCount += 1;
+          });
+      });
+      await Promise.all(sending);
+      return res
+        .status(200)
+        .send(
+          `Message sent successfully ${successCount}, failed messages: ${failCount}`
+        );
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
 module.exports = {
   createFuneral,
   editFuneral,
@@ -137,4 +173,5 @@ module.exports = {
   getSingleFuneral,
   deleteFuneral,
   getFuneralIds,
+  sendMessages,
 };
--- modules/funerals/funeral.route.js
+++ modules/funerals/funeral.route.js
@@ -6,6 +6,7 @@ const {
   getSingleFuneral,
   deleteFuneral,
   getFuneralIds,
+  sendMessages,
 } = require("./funeral.controller");
 
 const funeralRouter = router();
@@ -16,6 +17,7 @@ funeralRouter.get("/all-funerals", getAllFunerals);
 funeralRouter.get("/all-ids", getFuneralIds);
 funeralRouter.get("/singleFuneral", getSingleFuneral);
 funeralRouter.delete("/delete", deleteFuneral);
+funeralRouter.post("/send-message", sendMessages);
 
 module.exports = {
   funeralRouter,
